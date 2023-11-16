07a03d9c58da6d833635eaebf7114b9276c25938
Henry Kolawole <you@example.com>
2023-11-16 15:45:21 +0100

    Material tailwind has been set up

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {Input, Button} from "@material-tailwind/react";
+
 
 function Home() {
   return (
-    <div className='flex justify-center pt-10 text-3xl text-green-500 hover:text-green-700 hover:font-bold'>Welcome Home</div>
+    <div>
+
+      <div className='flex justify-center pt-10 text-3xl text-green-500 hover:text-green-700 hover:font-bold'>Welcome Home</div>
+
+     
+
+
+      <div className='flex justify-center mt-10'>
+
+
+        <Button className=' bg-green-500 hover:bg-red-500'>Button</Button>
+
+
+      </div>
+
+
+    </div>
   )
 }
 
