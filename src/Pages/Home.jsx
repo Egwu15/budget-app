226fa2ccde9820c56f7ce6fa1fdc8b4be2da56a1
Henry Kolawole <you@example.com>
2023-11-16 15:16:11 +0100

    Tailwind has been set up

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='flex justify-center pt-10 text-3xl text-green-500 hover:text-green-700 hover:font-bold'>Welcome Home</div>
+  )
+}
+
+export default Home
